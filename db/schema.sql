2b18c720f77dcc95eeb342e202cac87d5316312b
Ricky Bobby <richard.pajewski@gmail.com>
2020-11-20 12:35:57 -0600

    generate database and seed files successful now

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE roles (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL UNSIGNED NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
     INDEX dep_index (department_id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE
 );
@@ -21,12 +21,12 @@ CREATE TABLE employees (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
     INDEX role_index (role_id),
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
     manager_id INT UNSIGNED,
     INDEX man_index (manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE CASCADE
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 );
 
 -- CREATE TABLE budgets (
